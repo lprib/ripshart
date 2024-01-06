3e5f6a09f588c1471cb28f93523e73b294e40ac8
Liam Pribis <jackpribis@gmail.com>
2024-01-06 03:28:06 +0000

    bez corner smoothing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <cstdio>
 
 #include <raylib.h>
+#include <vector>
 
 #include "line.hpp"
+
+static Line gen_random_line() {
+   std::vector<Station> stations{};
+   for(int i = 0; i < 20; ++i) {
+      stations.push_back(
+         Station(GetRandomValue(10, 630), GetRandomValue(10, 470))
+      );
+   }
+
+   return Line(stations, ORANGE);
+}
 
 int main(int argc, char** argv) {
    InitWindow(640, 480, "ripshart unshackled");
@@ -11,11 +23,18 @@ int main(int argc, char** argv) {
    auto line =
       Line({Station(100, 100), Station(200, 100), Station(500, 200)}, BLUE);
 
+   auto line2 = gen_random_line();
+
    while(!WindowShouldClose()) {
+      if(IsKeyPressed(KEY_SPACE)) {
+         line2 = gen_random_line();
+      }
+
       BeginDrawing();
       {
          line.stations[2].pos = GetMousePosition();
          ClearBackground(WHITE);
+         line2.draw();
          line.draw();
       }
       EndDrawing();
